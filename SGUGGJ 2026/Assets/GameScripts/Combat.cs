1c176f6299bcbcda923d44bd68d0cecf6e477c5d
BUGISOFT <sigmagames763@gmail.com>
2026-01-30 16:14:44 -0300

    Update Combat.cs

--- SGUGGJ 2026/Assets/GameScripts/Combat.cs
+++ SGUGGJ 2026/Assets/GameScripts/Combat.cs
@@ -17,7 +17,7 @@ public class Combat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
 
             attack();
         }
